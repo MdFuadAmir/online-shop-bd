8a513f0dd6eea4d576d6f240cafeadc34c61f8e8
Md Fuad Amir <mdfuadamir@gmail.com>
2024-04-01 03:03:40 +0600

    added setup

---
+++ src/Root/Root.jsx
@@ -0,0 +1,12 @@
+import Home from "../Components/Home/Home";
+
+
+const Root = () => {
+    return (
+        <div className="mx-auto max-w-7xl p-2"> 
+            <Home></Home>
+        </div>
+    );
+};
+
+export default Root;
